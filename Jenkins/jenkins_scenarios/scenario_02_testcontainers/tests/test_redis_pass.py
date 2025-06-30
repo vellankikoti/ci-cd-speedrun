a0d814240da932702c85b7ab3bdaa054c3f617f2
vellankikoti <vellankikoti@gmail.com>
2025-06-30 22:26:32 +0530

    added jenkins scenario 2

--- Jenkins/jenkins_scenarios/scenario_02_testcontainers/tests/test_redis_pass.py
+++ Jenkins/jenkins_scenarios/scenario_02_testcontainers/tests/test_redis_pass.py
@@ -1,16 +1,16 @@
 import redis
-import pytest
 from testcontainers.redis import RedisContainer
 
-def test_connect_redis_container():
-    with RedisContainer("redis:7.2") as redis_container:
+def test_redis_container():
+    with RedisContainer("redis:7.0.5").with_bind_ports(6379, 6379) as redis_container:
         host = redis_container.get_container_host_ip()
         port = redis_container.get_exposed_port(6379)
 
-        client = redis.Redis(
-            host=host,
-            port=int(port),
-            db=0
-        )
-        pong = client.ping()
-        assert pong is True
+        print(f"Redis running on {host}:{port}")
+
+        client = redis.StrictRedis(host=host, port=int(port), decode_responses=True)
+        client.set("mykey", "myvalue")
+        value = client.get("mykey")
+
+        print("Redis GET mykey:", value)
+        assert value == "myvalue"
